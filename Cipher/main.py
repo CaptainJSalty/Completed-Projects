2aaa90cae8e168ff1f559cdcc891c0a647e43080
wnwogus357@gmail.com <wnwogus357@gmail.com>
2022-11-24 01:39:06 -0500

    moving completed projects to github

---
+++ Cipher/main.py
@@ -0,0 +1,35 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar (plain_text, shift_amount, direction_code): 
+
+  end_text = ""
+  if direction_code == "decode":
+      shift_amount *= -1 
+  for letter in plain_text: 
+    position = alphabet.index(letter)
+    
+    new_position = position + shift_amount 
+    end_text += alphabet[new_position]
+
+  print(f"The {direction_code}d text is {end_text}")
+
+
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+  #Try running the program and entering a shift number of 45.
+  #Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+  #Hint: Think about how you can use the modulus (%).
+  shift = shift % 26
+
+  caesar(plain_text=text, shift_amount=shift, direction_code=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
